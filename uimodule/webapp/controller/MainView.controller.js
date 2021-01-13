5dbb28860f9f22c04ad923dd30fd62ca23e298be
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2021-01-13 10:37:19 +0100

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "net/prp/dienstleister/fe/prpFESample/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("net.prp.dienstleister.fe.prpFESample.controller.MainView", {});
+});
